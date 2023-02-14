73e6481874155afe92e440e4932dc24e22fc130c
icemanparachute <acfmichelsen@gmail.com>
2023-02-13 20:02:23 -0500

    remove other futuresNFT

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import '../interfaces/IWETH.sol';
+
 
 /// @notice Library to help safely transfer tokens and handle ETH wrapping and unwrapping of WETH
 library TransferHelper {
@@ -44,46 +44,4 @@ library TransferHelper {
     require(postBalance - priorBalance == amount, 'THL02');
   }
 
-  /// @dev Internal function that handles transfering payments from buyers to sellers with special WETH handling
-  /// @dev this function assumes that if the recipient address is a contract, it cannot handle ETH - so we always deliver WETH
-  /// @dev special care needs to be taken when using contract addresses to sell deals - to ensure it can handle WETH properly when received
-  function transferPayment(
-    address weth,
-    address token,
-    address from,
-    address payable to,
-    uint256 amount
-  ) internal {
-    if (token == weth) {
-      require(msg.value == amount, 'THL03');
-      if (!Address.isContract(to)) {
-        (bool success, ) = to.call{value: amount}('');
-        require(success, 'THL04');
-      } else {
-        /// @dev we want to deliver WETH from ETH here for better handling at contract
-        IWETH(weth).deposit{value: amount}();
-        assert(IWETH(weth).transfer(to, amount));
-      }
-    } else {
-      transferTokens(token, from, to, amount);
-    }
-  }
-
-  /// @dev Internal funciton that handles withdrawing tokens and WETH that are up for sale to buyers
-  /// @dev this function is only called if the tokens are not timelocked
-  /// @dev this function handles weth specially and delivers ETH to the recipient
-  function withdrawPayment(
-    address weth,
-    address token,
-    address payable to,
-    uint256 amount
-  ) internal {
-    if (token == weth) {
-      IWETH(weth).withdraw(amount);
-      (bool success, ) = to.call{value: amount}('');
-      require(success, 'THL04');
-    } else {
-      withdrawTokens(token, to, amount);
-    }
-  }
 }
