6817b36b64d051c714ec11d009cd01c4518dca83
icemanparachute <acfmichelsen@gmail.com>
2023-03-09 12:43:59 -0500

    add natspec to batch contracts

--- contracts/BatchStreamer.sol
+++ contracts/BatchStreamer.sol
@@ -4,8 +4,20 @@ pragma solidity 0.8.17;
 import './interfaces/IStreamNFT.sol';
 import './libraries/TransferHelper.sol';
 
+/// @title BatchStreamer is a simple contract to create multiple streaming NFTs in a single
+/// this contract is used as a layer on top of the StreamingNFT core contract
+
 contract BatchStreamer {
   event BatchCreated(uint256 mintType);
+
+  /// @notice craeate a batch of streamingNFTs with the same token to various recipients, amounts, start dates, cliffs and rates
+  /// @param streamer is the address of the StreamingNFT contrac this points to, either the StreamingHedgeys or the StreamingBoundHedgeys
+  /// @param recipients is the array of addresses for those wallets receiving the streams
+  /// @param token is the address of the token to be locked inside the NFTs and linearly unlocked to the recipients
+  /// @param amounts is the array of the amount of tokens to be locked in each NFT, each directly related in sequence to the recipient and other arrays
+  /// @param starts is the array of start dates that define when each NFT will begin linearly unlocking
+  /// @param cliffs is the array of cliff dates that define each cliff date for the NFT stream
+  /// @param rates is the array of per second rates that each NFT will unlock at the rate of
 
   function createBatch(
     address streamer,
@@ -23,6 +35,17 @@ contract BatchStreamer {
     }
     _createBatch(streamer, recipients, token, amounts, totalAmount, starts, cliffs, rates);
   }
+
+  /// @notice craeate a batch of streamingNFTs with the same token to various recipients, amounts, start dates, cliffs and rates
+  /// this contract has a special event emitted based on the mintType param
+  /// @param streamer is the address of the StreamingNFT contrac this points to, either the StreamingHedgeys or the StreamingBoundHedgeys
+  /// @param recipients is the array of addresses for those wallets receiving the streams
+  /// @param token is the address of the token to be locked inside the NFTs and linearly unlocked to the recipients
+  /// @param amounts is the array of the amount of tokens to be locked in each NFT, each directly related in sequence to the recipient and other arrays
+  /// @param starts is the array of start dates that define when each NFT will begin linearly unlocking
+  /// @param cliffs is the array of cliff dates that define each cliff date for the NFT stream
+  /// @param rates is the array of per second rates that each NFT will unlock at the rate of
+  /// @param mintType is an internal identifier used by Hedgey Applications to record special identifiers for special metadata creation and internal analytics tagging
 
   function createBatch(
     address streamer,
@@ -43,6 +66,9 @@ contract BatchStreamer {
     _createBatch(streamer, recipients, token, amounts, totalAmount, starts, cliffs, rates);
   }
 
+  /// @notice _createBatch is the internal function called by the external createBatch functions
+  /// it checks all of the arrays are the same length
+  /// and takes one additional input, the total amount, which it then pulls into the contract to then mint each NFT to the recipient
   function _createBatch(
     address streamer,
     address[] memory recipients,
@@ -58,7 +84,7 @@ contract BatchStreamer {
         amounts.length == starts.length &&
         starts.length == cliffs.length &&
         cliffs.length == rates.length,
-        'array length error'
+      'array length error'
     );
     TransferHelper.transferTokens(token, msg.sender, address(this), totalAmount);
     SafeERC20.safeIncreaseAllowance(IERC20(token), streamer, totalAmount);
