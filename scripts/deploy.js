1779a7cb1f485ee1fa7817a82be6c4f31785678d
icemanparachute <acfmichelsen@gmail.com>
2023-03-16 15:39:26 -0400

    update deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ const streamingNFT = require('../artifacts/contracts/StreamingHedgeys.sol/Stream
 const streamingBound = require('../artifacts/contracts/StreamingBoundHedgeys.sol/StreamingBoundHedgeys.json');
 const keys = require('./keys');
 
-async function deployNFT(rpcUrl, artifact, privateKey, args) {
+async function deployNFT(rpcUrl, artifact, privateKey, args, uriBase) {
     const rpc = rpcUrl;
     const provider = new ethers.providers.JsonRpcProvider(rpc);
     const wallet = new ethers.Wallet(privateKey, provider);
@@ -15,7 +15,7 @@ async function deployNFT(rpcUrl, artifact, privateKey, args) {
     const contract = await factory.deploy(...args);
     await contract.deployed();
     console.log(contract.address);
-    const uri = `https://mthcf2zxy4nudhjmubecmgkch40aeeqt.lambda-url.us-east-1.on.aws/goerli/${contract.address.toLowerCase()}/`;
+    const uri = `${uriBase}${contract.address.toLowerCase()}/`;
     await contract.updateBaseURI(uri);
     console.log(args);
 }
@@ -31,24 +31,10 @@ async function deploybatch(rpcUrl, artifact, privateKey) {
     console.log(contract.address);
 }
 
-async function updateURI(rpcUrl, artifact, privateKey, address) {
+async function updateURI(rpcUrl, artifact, privateKey, address, uriBase) {
     const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
     const wallet = new ethers.Wallet(privateKey, provider);
     const contract = new ethers.Contract(address, artifact.abi, wallet);
-    const uri = `https://mthcf2zxy4nudhjmubecmgkch40aeeqt.lambda-url.us-east-1.on.aws/goerli/${address.toLowerCase()}/`;
+    const uri = `${uriBase}${address.toLowerCase()}/`;
     await contract.updateBaseURI(uri);
 }
-
-const st = '0xe4ade6d8db85f67b7181411ae2109ca27090ae4c';
-const bound = '0x2B1507213665757E62a7D7e00dc88FD38F52fd2e';
-const vs = '0x6d6E83ea5Db3534e25c5807bfbE71a0c8cDDb85a';
-const batchVs = '0x37308b9701A4406e99f2EEF9C86247f01755156C';
-const batchSt = '0x9521fD51cC3dA95B4d254CB73b3a3D77e3a86979';
-// deploys the contracts
-// deployNFT(keys.goerliURL, streamingNFT, keys.wallets.d.privateKey, ['StreamingHedgeys', 'STRMHEDGY']);
-// deployNFT(keys.goerliURL, streamingBound, keys.wallets.d.privateKey, ['StreamingBoundHedgeys', 'BOUNDHGY']);
-// deployNFT(keys.goerliURL, vestingNFT, keys.wallets.d.privateKey, ['Vesting_Hedgeys', 'VESTHDG', st, bound]);
-// use npx hardhat verify --network [network] [contract_address] [args] to verify on etherscan
-
-//deploybatch(keys.goerliURL, batchStreamer, keys.wallets.d.privateKey);
-
